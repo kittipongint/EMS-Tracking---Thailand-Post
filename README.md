a04ff0b4c65028ae64995f0085b2c90d3802e4db
kittipongint <kittipongint@hotmail.com>
2017-03-03 15:07:26 +0700

    Initial project.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # EMS-Tracking---Thailand-Post
+
+Sample web service caller of Thailand Post - Track&Trace Web Service
+
+#Requirement
+1.Visual Studio (2015)
+2.Internet connection
+
+#How to
+1. Replace username & password which you recieve from Thailand Post.
+2. Replace Barcode with valid EMS number.
+3. Run and view result.
---
+++ ThaiPost/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThaiPost.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string Username = ""; // Username from Thailand Post
+        private string Password = ""; // Password from Thailand Post
+        private string Languege = "TH"; //"EN"
+        private string Barcode = "ER787920xxxTH";
+        private string Barcodes = "ER787920xxxTH,ER438917xxxTH,ER438906xxxTH";
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult EMSTracking()
+        {
+
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            string xmlKey = rsa.ToXmlString(false);
+            string xmlPrivKey = rsa.ToXmlString(true);
+
+            thailandpost.track.TrackandTraceService sParams = new thailandpost.track.TrackandTraceService();
+            sParams.Url = " http://track.thailandpost.co.th/TTPOSTWebService/TrackandTrace.asmx";
+            //sParams.Proxy
+
+            sParams.PublicKeySoapHeaderValue = new thailandpost.track.PublicKeySoapHeader();
+            sParams.PublicKeySoapHeaderValue.PublicXmlKey = xmlKey;
+            thailandpost.track.TrackItem item = sParams.GetItems(Username, Password, Languege, Barcode);
+            //if (item.ItemsData.Length > 0)
+            //{
+            //}
+
+            ViewBag.Barcode = Barcode;
+
+            return View(item);
+        }
+
+        public ActionResult EMSTrackingBatch()
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            string xmlKey = rsa.ToXmlString(false);
+            string xmlPrivKey = rsa.ToXmlString(true);
+
+            thailandpost.track.TrackandTraceService sParams = new thailandpost.track.TrackandTraceService();
+            sParams.Url = " http://track.thailandpost.co.th/TTPOSTWebService/TrackandTrace.asmx";
+            //sParams.Proxy
+
+            sParams.PublicKeySoapHeaderValue = new thailandpost.track.PublicKeySoapHeader();
+            sParams.PublicKeySoapHeaderValue.PublicXmlKey = xmlKey;
+
+            string strQuery = Barcodes;
+            strQuery = strQuery.Trim().Replace("\r\n", ",");
+            strQuery = strQuery.Replace(",,", ",");
+            if (strQuery.Substring(strQuery.Length - 1, 1) == ",")
+                strQuery = strQuery.Substring(0, strQuery.Length - 1);
+
+            string[] ItemArr = strQuery.Split(',');
+
+            thailandpost.track.TrackRequest item = sParams.RequestItems(Username, Password, Languege, ItemArr);
+
+            //if (item.ItemsData.Length > 0)
+            //{
+            //}
+
+            ViewBag.Barcode = Barcode;
+            ViewBag.BarcodeCount = item.RequestCountData.CountNumber;
+
+            return View(item);
+        }
+    }
+}
